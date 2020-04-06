d6f46f3582eaed4f66e1fd4a09f1105daea7ef7d
Gonzalo Beviglia <gonchu.b@gmail.com>
2020-04-06 11:41:35 +0200

    SVG action

--- src/components/EmailTag.ts
+++ src/components/EmailTag.ts
@@ -15,8 +15,20 @@ export function EmailTag(props: EmailTagProps): Component {
     emailElement.setAttribute("id", IDS.EMAIL_TAG(props.email.id));
 
     const emailActionElement = document.createElement("span");
-    emailActionElement.setAttribute("role", "button")
-    emailActionElement.setAttribute("tabindex", "0")
+    emailActionElement.setAttribute("role", "button");
+    emailActionElement.setAttribute("tabindex", "0");
+
+    const svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const svgPath = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+
+    svgElement.setAttribute("aria-hidden","true");
+    svgElement.setAttribute('viewbox', '0 0 8 8');
+    svgElement.setAttribute('width', '8px');
+    svgElement.setAttribute('height', '8px');
+    svgElement.setAttribute('fill', 'none')
+    svgPath.setAttribute('d', 'M8 0.8L7.2 0L4 3.2L0.8 0L0 0.8L3.2 4L0 7.2L0.8 8L4 4.8L7.2 8L8 7.2L4.8 4L8 0.8Z');
+    svgPath.setAttribute('fill', '#050038');
+    svgElement.appendChild(svgPath);
 
     function handleDelete(): void {
         props.onDelete(props.email.id);
@@ -40,7 +52,7 @@ export function EmailTag(props: EmailTagProps): Component {
 
         emailElement.setAttribute("class", className);
         emailActionElement.setAttribute("class", CLASS_NAMES.EMAIL_TAG_ACTION);
-        emailActionElement.appendChild(document.createTextNode("X"))
+        emailActionElement.appendChild(svgElement)
 
         emailActionElement.addEventListener("click", handleClick);
         emailActionElement.addEventListener("keypress", handleInputKeyPress);
